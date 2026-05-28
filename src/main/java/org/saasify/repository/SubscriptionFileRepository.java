fedce6ab163a152838006d7f3d57e3a1d55ee9e5
DaniAlc12 <danialcaniz12@gmail.com>
2026-05-28 18:54:34 +0200

    commit_repo_subscriptionRepo

---
+++ src/main/java/org/saasify/repository/SubscriptionFileRepository.java
@@ -0,0 +1,96 @@
+package org.saasify.repository;
+
+import org.saasify.models.Client;
+import org.saasify.models.Subscription;
+import org.saasify.models.SubscriptionData;
+import org.saasify.models.SubscriptionPlan;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class SubscriptionFileRepository implements SubscriptionRepository{
+    private final String filePath;
+    private final SubscriptionPlanRepository planRepository;
+    private final ClientRepository clientRepository;
+    private final List<Subscription> subscriptions;
+
+    public SubscriptionFileRepository(String filePath, SubscriptionPlanRepository planRepository, ClientRepository clientRepository) {
+        this.filePath = filePath;
+        this.planRepository = planRepository;
+        this.clientRepository = clientRepository;
+        this.subscriptions = deserializeSubscriptions();
+    }
+
+    public List<Subscription> deserializeSubscriptions() {
+        File file = new File(filePath);
+        if(!file.exists()){
+            return new ArrayList<>();
+        }
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))){
+            List<SubscriptionData> subscriptionData = (List<SubscriptionData>) ois.readObject();
+            List<Subscription> realSubscriptions = new ArrayList<>();
+            for(SubscriptionData data : subscriptionData){
+
+                Optional<Client> optionalClient = this.clientRepository.findByDni(data.getClientDni());
+                if(!optionalClient.isPresent()){
+                    throw new IllegalArgumentException("the dni is not found");
+                }
+                Client client = optionalClient.get();
+
+                Optional<SubscriptionPlan> optionalPlan = this.planRepository.findById(data.getPlanId());
+                if(!optionalPlan.isPresent()){
+                    throw new IllegalArgumentException("the plan is not found");
+                }
+                SubscriptionPlan plan = optionalPlan.get();
+
+                Subscription sub = new Subscription(
+                        client,
+                        plan,
+                        data.getStartDate(),
+                        data.getState(),
+                        data.getNextBillingDate()
+                );
+                realSubscriptions.add(sub);
+            }
+            return realSubscriptions;
+        }catch (IOException | ClassNotFoundException e){
+            System.err.println("Error deserializing Subscription plans.");
+            return new ArrayList<>();
+        }
+    }
+
+    private void serializeSubscriptions(){
+        List<SubscriptionData> subscriptionData = new ArrayList<>();
+        for(Subscription sub : this.subscriptions){
+            subscriptionData.add(new SubscriptionData(sub));
+        }
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))){
+            oos.writeObject(subscriptionData);
+        }catch(IOException e){
+            throw new RuntimeException("Error serializando clientes.");
+        }
+    }
+
+    @Override
+    public void save(Subscription subscription) {
+        subscriptions.add(subscription);
+        serializeSubscriptions();
+    }
+
+    @Override
+    public Optional<Subscription> findByClientDni(String dni) {
+        for(Subscription sub : subscriptions){
+            if(sub.getClient().getDni().equals(dni)){
+                return Optional.of(sub);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Subscription> findAll() {
+        return this.subscriptions;
+    }
+}
---
+++ src/main/java/org/saasify/repository/SubscriptionRepository.java
@@ -0,0 +1,12 @@
+package org.saasify.repository;
+
+import org.saasify.models.Subscription;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubscriptionRepository {
+    void save(Subscription subscription);
+    Optional<Subscription> findByClientDni(String dni);
+    List<Subscription> findAll();
+}
