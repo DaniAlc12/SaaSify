cf001219a824c75a6cecc50b188ecf182d848849
DaniAlc12 <danialcaniz12@gmail.com>
2026-05-28 16:43:11 +0200

    commit_clientFile

---
+++ src/main/java/org/saasify/repository/ClientFileRepository.java
@@ -0,0 +1,68 @@
+package org.saasify.repository;
+
+import org.saasify.models.Client;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ClientFileRepository implements ClientRepository {
+
+    private final String filePath;
+    private final List<Client> clients;
+
+    public ClientFileRepository(String filePath) {
+        if(filePath == null || filePath.isBlank()) {
+            throw new IllegalArgumentException("filePath is null or blank");
+        }
+        this.filePath = filePath;
+        this.clients = deserializarClientes();
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Client> deserializarClientes() {
+        File file = new File(filePath);
+
+        if(!file.exists()) {
+            return new ArrayList<>();
+        }
+
+        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath))){
+            return (List<Client>) ois.readObject();
+        }catch (IOException | ClassNotFoundException e){
+            System.err.println("Error deserializando clientes.");
+            return new ArrayList<>();
+        }
+    }
+
+    private void serializarClientes(){
+        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath))){
+            oos.writeObject(clients);
+        }catch(IOException e){
+            throw new RuntimeException("Error serializando clientes.");
+        }
+    }
+
+    @Override
+    public Optional<Client> findByDni(String dni) {
+        for(Client client : clients) {
+            if(client.getDni().equals(dni)) {
+                return Optional.of(client);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return clients;
+    }
+
+    @Override
+    public void save(Client client) {
+        clients.add(client);
+        serializarClientes();
+    }
+}
+
