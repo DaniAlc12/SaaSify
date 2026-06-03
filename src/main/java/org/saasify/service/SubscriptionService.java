1a52e55805573a9e2d3a276e2604e7b890ff4a4a
DaniAlc12 <danialcaniz12@gmail.com>
2026-06-03 15:33:17 +0200

    commit_service_processBilling

---
+++ src/main/java/org/saasify/service/SubscriptionService.java
@@ -0,0 +1,40 @@
+package org.saasify.service;
+
+import org.saasify.exceptions.InsufficientFundsException;
+import org.saasify.models.Client;
+import org.saasify.models.Subscription;
+import org.saasify.models.SubscriptionPlan;
+import org.saasify.models.SubscriptionState;
+import org.saasify.repository.ClientRepository;
+import org.saasify.repository.SubscriptionPlanRepository;
+import org.saasify.repository.SubscriptionRepository;
+
+import java.time.LocalDate;
+
+public class SubscriptionService {
+    private final SubscriptionRepository subscriptionRepository;
+    private final ClientRepository clientRepository;
+    private final SubscriptionPlanRepository subscriptionPlanRepository;
+
+    public SubscriptionService(SubscriptionRepository subscriptionRepository, ClientRepository clientRepository, SubscriptionPlanRepository subscriptionPlanRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+        this.clientRepository = clientRepository;
+        this.subscriptionPlanRepository = subscriptionPlanRepository;
+    }
+
+    public void processBilling(String clientDni, int planId){
+        Client client = clientRepository.findByDni(clientDni).orElseThrow(()->new RuntimeException("Client not found"));
+        SubscriptionPlan subsPlan= subscriptionPlanRepository.findById(planId).orElseThrow();
+
+        if(client.getBankBalance().compareTo(subsPlan.price()) < 0){
+            throw new InsufficientFundsException("Insufficient Funds");
+        }else{
+            client.setBankBalance(client.getBankBalance().subtract(subsPlan.price()));
+            clientRepository.save(client);
+            Subscription subscription = new Subscription(client,subsPlan, LocalDate.now(), SubscriptionState.ACTIVE);
+            subscriptionRepository.save(subscription);
+        }
+
+
+    }
+}
